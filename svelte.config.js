8a8626cf1e07d7b54313470fc29d990a594fa593
Tyler <ty@tyboyd.dev>
2021-12-10 14:27:30 -0600

    added vercel adapter for deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import preprocess from 'svelte-preprocess';
+import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,7 +7,8 @@ const config = {
 
 	kit: {
 		target: '#svelte',
-		lib: 'src'
+		lib: 'src',
+		adapter: vercel()
 	}
 };
 
