727343875d019b87be6845ed6cf37906b28386ce
Tyler <ty@tyboyd.dev>
2021-12-10 14:42:48 -0600

    changed the vercel import to node

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import vercel from '@sveltejs/adapter-vercel';
+import node from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +8,7 @@ const config = {
 	kit: {
 		target: '#svelte',
 		lib: 'src',
-		adapter: vercel()
+		adapter: node()
 	}
 };
 
