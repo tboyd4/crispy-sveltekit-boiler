ce2907835034eb1c4e16ddec4d7292a6c42a0c39
Tyler Boyd <ty@tyboyd.dev>
2022-07-14 14:24:54 -0500

    removing .vercel

--- .vercel/output/functions/render.func/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,7 +0,0 @@
-import * as module from '../entries/pages/__layout.svelte.js';
-
-export { module };
-export const index = 0;
-export const file = 'immutable/pages/__layout.svelte-908b6ba5.js';
-export const imports = ["immutable/pages/__layout.svelte-908b6ba5.js","immutable/chunks/index-bf1ed64f.js"];
-export const stylesheets = ["immutable/assets/__layout-b4f99ace.css"];
--- .vercel/output/functions/render.func/.svelte-kit/output/server/nodes/2.js
+++
@@ -1,7 +0,0 @@
-import * as module from '../entries/pages/index.svelte.js';
-
-export { module };
-export const index = 2;
-export const file = 'immutable/pages/index.svelte-6315cce0.js';
-export const imports = ["immutable/pages/index.svelte-6315cce0.js","immutable/chunks/index-bf1ed64f.js"];
-export const stylesheets = [];
